4fea4f3057682c398903559122a138991472c884
MikhaelTatischev <tatischevm@gmail.com>
2023-01-19 19:07:34 +0300

    feat: класс customlinkedlist сделан вложенным классом

--- src/service/impl/InMemoryHistoryManager.java
+++ src/service/impl/InMemoryHistoryManager.java
@@ -10,71 +10,8 @@ import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final static int HISTORY_SIZE = 10;
-    private Map<Integer, Node> mapOfNodes = new HashMap<>();
     private List<Task> viewedTasks = new ArrayList<>();
-    private Node head;
-    private Node tail;
-    private int size = 0;
-
-    public Map<Integer, Node> getTasks() {
-        return mapOfNodes;
-    }
-
-    public Node linkLast(Task task) {
-        Node oldTail = tail;
-        Node newTail = new Node(task);
-
-        if (size == 0) {
-            tail = newTail;
-            head = tail;
-        } else {
-            tail.nextNode = newTail;
-            newTail.prevNode = oldTail;
-            tail = newTail;
-        }
-
-        mapOfNodes.put(tail.task.getId(), tail);
-        size++;
-
-        return tail;
-    }
-
-    public Node removeNode(Node node) {
-        Node nodeToBeRemoved = node;
-
-        if (nodeToBeRemoved == null) {
-            System.out.println("Такой задачи нет");
-            return null;
-        }
-
-        if (size == 1) {
-            head = null;
-            tail = null;
-            nodeToBeRemoved.nextNode = null;
-            nodeToBeRemoved.prevNode = null;
-            mapOfNodes.remove(nodeToBeRemoved);
-            size--;
-            return nodeToBeRemoved;
-        }
-
-        if (nodeToBeRemoved.prevNode == null) {
-            nodeToBeRemoved.nextNode.prevNode = null;
-            head = nodeToBeRemoved.nextNode;
-        } else if (nodeToBeRemoved.nextNode == null) {
-            nodeToBeRemoved.prevNode.nextNode = null;
-            tail = nodeToBeRemoved.prevNode;
-        } else {
-            nodeToBeRemoved.prevNode.nextNode = nodeToBeRemoved.nextNode;
-            nodeToBeRemoved.nextNode.prevNode = nodeToBeRemoved.prevNode;
-            nodeToBeRemoved.prevNode = null;
-            nodeToBeRemoved.nextNode = null;
-        }
-
-        mapOfNodes.remove(nodeToBeRemoved);
-        size--;
-
-        return nodeToBeRemoved;
-    }
+    CustomLinkedList customLinkedList = new CustomLinkedList();
 
     @Override
     public void add(Task task) {
@@ -86,20 +23,20 @@ public class InMemoryHistoryManager implements HistoryManager {
             viewedTasks.remove(0);
         }
 
-        linkLast(task);
+        customLinkedList.linkLast(task);
         viewedTasks.add(task);
     }
 
     @Override
     public void remove(int id) {
-        if (!mapOfNodes.containsKey(id)) {
+        if (!customLinkedList.mapOfNodes.containsKey(id)) {
             return;
         }
 
-        Task taskToBeRemove = mapOfNodes.get(id).task;
+        Task taskToBeRemove = customLinkedList.mapOfNodes.get(id).task;
         viewedTasks.remove(taskToBeRemove);
-        removeNode(mapOfNodes.get(id));
-        mapOfNodes.remove(id);
+        customLinkedList.removeNode(customLinkedList.mapOfNodes.get(id));
+        customLinkedList.mapOfNodes.remove(id);
     }
 
     @Override
@@ -110,8 +47,75 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public void removeAllTasks() {
         viewedTasks.clear();
-        mapOfNodes.clear();
-        head = null;
-        tail = null;
+        customLinkedList.mapOfNodes.clear();
+        customLinkedList.head = null;
+        customLinkedList.tail = null;
+    }
+
+    private class CustomLinkedList {
+        private Node head;
+        private Node tail;
+        private int size = 0;
+        private Map<Integer, Node> mapOfNodes = new HashMap<>();
+
+        public Map<Integer, Node> getTasks() {
+            return mapOfNodes;
+        }
+
+        public Node linkLast(Task task) {
+            Node oldTail = tail;
+            Node newTail = new Node(task);
+
+            if (size == 0) {
+                tail = newTail;
+                head = tail;
+            } else {
+                tail.nextNode = newTail;
+                newTail.prevNode = oldTail;
+                tail = newTail;
+            }
+
+            mapOfNodes.put(tail.task.getId(), tail);
+            size++;
+
+            return tail;
+        }
+
+        public Node removeNode(Node node) {
+            Node nodeToBeRemoved = node;
+
+            if (nodeToBeRemoved == null) {
+                System.out.println("Такой задачи нет");
+                return null;
+            }
+
+            if (size == 1) {
+                head = null;
+                tail = null;
+                nodeToBeRemoved.nextNode = null;
+                nodeToBeRemoved.prevNode = null;
+                mapOfNodes.remove(nodeToBeRemoved);
+                size--;
+                return nodeToBeRemoved;
+            }
+
+            if (nodeToBeRemoved.prevNode == null) {
+                nodeToBeRemoved.nextNode.prevNode = null;
+                head = nodeToBeRemoved.nextNode;
+            } else if (nodeToBeRemoved.nextNode == null) {
+                nodeToBeRemoved.prevNode.nextNode = null;
+                tail = nodeToBeRemoved.prevNode;
+            } else {
+                nodeToBeRemoved.prevNode.nextNode = nodeToBeRemoved.nextNode;
+                nodeToBeRemoved.nextNode.prevNode = nodeToBeRemoved.prevNode;
+                nodeToBeRemoved.prevNode = null;
+                nodeToBeRemoved.nextNode = null;
+            }
+
+            mapOfNodes.remove(nodeToBeRemoved);
+            size--;
+
+            return nodeToBeRemoved;
+        }
     }
 }
